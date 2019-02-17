685bba0d2294f43379c0d4cc697fa4b461b9b111
Dean Cuk <dean.cuk89@gmail.com>
2019-02-17 16:08:18 +0100

    remove httpmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { ArticleComponent } from './article/article.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     RouterModule
   ],
   providers: [],
